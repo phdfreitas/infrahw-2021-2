6db4ca57e76ee7bac6fb243615d3cfe7622e6511
phdfreitas <pedro.hnd.freitas@gmail.com>
2022-02-18 18:20:27 -0300

    Adicionando comentários Q4

--- q4.asm
+++ q4.asm
@@ -1,8 +1,8 @@
 .data
-B: .word 2
-E: .word 1
-I: .word 0
-RES: .word 5
+B: .word 2     # int b = 2
+E: .word 1     # int e = 1
+RES: .word 5   # res = 1
+I: .word 0     # int i = 0
 
 .text
 lw $t0, B 
@@ -10,15 +10,13 @@ lw $t1, E
 lw $t3, I
 lw $t4, RES
 
-slt $t5, $t3, $t1
-bne $t5, $zero, for
+slt $t5, $t3, $t1    # if (I < E) $t5 = 1
+bne $t5, $zero, for  # if ($t5 != 0), vai para o for
 
 for:
-mul $t4, $t4, $t0
-addi $t3, $t3, 1
-bne $t3, $t1, for
-beq $t3, $t1, fim
+mul $t4, $t4, $t0  # res *= 2 (nesse caso, o 2 esta sendo representado pelo B)
+addi $t3, $t3, 1   # i++
+bne $t3, $t1, for  # caso i != e, chamo o for novamente.
+beq $t3, $t1, fim  # e caso seja, pulo para encerrar o programa
 
 fim:
-
-
