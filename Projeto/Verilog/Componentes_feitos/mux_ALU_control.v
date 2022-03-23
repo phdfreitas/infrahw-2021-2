703fe015f9f2123f6d00e739906636db285a09b4
Daniel Cavalcante <dvictor.cintra@gmail.com>
2022-03-23 03:21:32 -0300

    mux de controle de sinal de operaçao da ALU

--- Projeto/Verilog/Componentes_feitos/mux_ALU_control.v
+++ Projeto/Verilog/Componentes_feitos/mux_ALU_control.v
@@ -1,14 +1,10 @@
 module mux_ALU_control (
-    input  wire    [1:0]   selector,
+    input  wire            selector,
     input  wire    [2:0]   ALU_op,
     input  wire    [5:0]   funct,
     output wire    [2:0]   data_out    // confirmar se é 3 bits na saida   
 );
 
-
-    
-    assign OX       = (selector[0]) ? 32'b00000000000000000000000000011101 : Data_0;
-    assign IX       = (selector[0]) ? Data_3[15-11] : 32'b00000000000000000000000000011111;
-    assign Data_out = (selector[1]) ? IX : OX;
+    assign Data_out = ? ALU_op : funct;
     
 endmodule
