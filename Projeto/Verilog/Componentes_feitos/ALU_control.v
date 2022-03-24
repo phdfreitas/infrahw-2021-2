180bce64567ae34f5b82c91b7b1ec7109c8e5ab6
Daniel Cavalcante <dvictor.cintra@gmail.com>
2022-03-24 00:42:17 -0300

    renamed

--- Projeto/Verilog/Componentes_feitos/ALU_control.v
+++ Projeto/Verilog/Componentes_feitos/ALU_control.v
@@ -1,10 +1,10 @@
-module mux_ALU_control (
+module ALU_control (
     input  wire            selector,
     input  wire    [2:0]   ALU_op,
     input  wire    [5:0]   funct,
     output wire    [2:0]   data_out    // confirmar se é 3 bits na saida   
 );
 
-    assign Data_out = ? ALU_op : funct;
+    assign Data_out = (selector) ? ALU_op : funct;
     
 endmodule
