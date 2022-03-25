b0b1dba71a3042f14d1aa3d90c57074982023294
phdfreitas <pedro.hnd.freitas@gmail.com>
2022-03-25 15:52:13 -0300

    Load Size

--- Projeto/Verilog/Componentes_feitos/load_size.v
+++ Projeto/Verilog/Componentes_feitos/load_size.v
@@ -1,17 +1,15 @@
 module load_size(
-    input wire  [1:0]   load_size_control //renomeei de selector pra ficar mais fácil,
+    input wire  [1:0]   load_size_control, 
     input wire  [31:0]  mdr_input,
-    output reg  [31:0]  load_size_out; // to usando "reg" ao invés de wire pq logo em seguida to usando always (tem no slide que é pra ser assim)
+    output wire  [31:0]  load_size_out;
 );
-    // tem que perguntar ao monitor "sempre" que o que acontecer isso aqui vai rodar :)
-    @always @(alguma coisa que eu não sei o que :3 ) begin
-        if(selector[1] == 0 && selector[0] == 0)
-            load_size_out <= mdr_input; // LW
-        else if(selector[1] == 0 && selector[0] == 1) begin
-            load_size_out <= {{16{0}}, mdr_input[15:0]}; // LH
-        end
-        else
-            load_size_out <= {{24{0}}, mdr_input[7:0]}; // LB
-    end
+    
+    wire [31:0] LHB; // valor de lh ou lb
+
+    // se o bit mais a direita for 1, então é lh, caso contrário é lw
+    assign LWH = (load_size_control[0]) ? {{16{0}}, mdr_input[15:0]} : mdr_input;
+
+    // caso o valor do bit mais a esquerda seja 1, então é lb, caso contrário, é o valor de lhb
+    assign load_size_out = (load_size_control[1]) ? {{24{0}}, mdr_input[7:0]} : LHB;  
 
 endmodule
