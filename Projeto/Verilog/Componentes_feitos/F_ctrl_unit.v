c5b3ff9ce9acd81750a3fa8870fed9af2624b905
Daniel Cavalcante <dvictor.cintra@gmail.com>
2022-04-03 18:04:24 -0300

    Adiantando algumas coisas (tudo comentado)

--- Projeto/Verilog/Componentes_feitos/F_ctrl_unit.v
+++ Projeto/Verilog/Componentes_feitos/F_ctrl_unit.v
@@ -58,8 +58,30 @@ parameter SUB               = 6'd6;
 parameter ADDI              = 6'd7;
 parameter END_R             = 6'd8;
 parameter END_IMMEDIATE     = 6'd9;
-parameter AND               = 6'd10; 
-
+parameter AND               = 6'd10;
+/*parameter DIV               = 6'd10;
+parameter MULT              = 6'd10;
+parameter JR                = 6'd10;
+parameter MFHI              = 6'd10;
+parameter MFLO              = 6'd10;
+parameter SLL               = 6'd10;
+parameter SLLV              = 6'd10;
+parameter SLT               = 6'd10;
+parameter SRA               = 6'd10;
+parameter SRAV              = 6'd10;
+parameter SRL               = 6'd10;
+parameter BREAK             = 6'd10;
+parameter RTE               = 6'd10;
+parameter ADDM              = 6'd10;
+//I FORMAT
+parameter ADDIU             = 6'd10;
+parameter BEQ               = 6'd10;
+parameter BNE               = 6'd10;
+parameter BLE               = 6'd10;
+parameter BGT               = 6'd10;
+parameter SLLM               = 6'd10;
+...
+*/
 
 initial begin
     STATE = INSTRUCTION_FETCH;
@@ -215,15 +237,119 @@ always @(posedge clk) begin
                         6'h24: begin
                             STATE = AND;
                         end
+                        /*6'h1a: begin
+                            STATE = DIV;
+                        end
+                        6'h18: begin
+                            STATE = MULT;
+                        end
+                        6'h8: begin
+                            STATE = JR;
+                        end
+                        6'h10: begin
+                            STATE = MFHI;
+                        end
+                        6'h12: begin
+                            STATE = MFLO;
+                        end
+                        6'h0: begin
+                            STATE = SLL;
+                        end
+                        6'h4: begin
+                            STATE = SLLV;
+                        end
+                        6'h2a: begin
+                            STATE = SLT;
+                        end
+                        6'h3: begin
+                            STATE = SRA;
+                        end
+                        6'h7: begin
+                            STATE = SRAV;
+                        end
+                        6'h2: begin
+                            STATE = SRL;
+                        end
+                        6'hd: begin
+                            STATE = BREAK;
+                        end
+                        6'h13: begin
+                            STATE = RTE;
+                        end
+                        6'h5: begin
+                            STATE = ADDM;
+                        end*/
                     endcase
                 end
-
+                // I type
                 6'h8: begin
                     STATE = ADDI;
                 end
 
-                // Fazer opCodes do tipo I
-                // Fazer opCodes do tipo J 
+                /*6'h9: begin
+                    STATE = ADDIU;
+                end
+
+                6'h4: begin
+                    STATE = BEQ;
+                end
+
+                6'h5: begin
+                    STATE = BNE;
+                end
+
+                6'h6: begin
+                    STATE = BLE;
+                end
+
+                6'h7: begin
+                    STATE = BGT;
+                end
+
+                6'h1: begin
+                    STATE = SLLM;
+                end
+
+                6'h20: begin
+                    STATE = LB;
+                end
+
+                6'h21: begin
+                    STATE = LH;
+                end
+
+                6'hf: begin
+                    STATE = LUI;
+                end
+
+                6'h23: begin
+                    STATE = LW;
+                end
+
+                6'h28: begin
+                    STATE = SB;
+                end
+
+                6'h29: begin
+                    STATE = SH;
+                end
+
+                6'ha: begin
+                    STATE = SLTI;
+                end
+
+                6'h2b: begin
+                    STATE = SW;
+                end
+                // J type 
+                6'h2: begin
+                    STATE = JUMP;
+                end
+
+                6'h3: begin
+                    STATE = JAL;
+                end*/
+
             endcase
         end
 
