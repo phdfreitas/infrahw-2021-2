62f855315056574f79a4a5e672c58c427e8fcc9f
phdfreitas <pedro.hnd.freitas@gmail.com>
2022-04-05 19:18:41 -0300

    BREAK

--- Projeto/Verilog/Componentes_feitos/F_ctrl_unit.v
+++ Projeto/Verilog/Componentes_feitos/F_ctrl_unit.v
@@ -344,6 +344,9 @@ always @(posedge clk) begin
                         R_FORMAT_SLT: begin
                             STATE = SLT;
                         end
+                        R_FORMAT_BREAK: begin
+                            STATE = BREAK;
+                        end
                     endcase
                 end
                 // I type
@@ -788,6 +791,37 @@ always @(posedge clk) begin
 
             STATE = ATRASA_PROX_INSTR;
         end
+
+        else if(STATE == BREAK) begin
+            PC_write            = 1'd1;
+            PC_write_cond       = 1'd0;
+            MEMRead             = 1'd0;
+            IRWrite             = 1'd0;
+            RegWrite            = 1'd0;
+            A_write             = 1'd0;
+            B_write             = 1'd0;
+            MDR_load            = 1'd0;
+            EPCWrite            = 1'd0;
+            AluOutWrite         = 1'd0;
+
+            RegDst              = 2'd0;
+            ALUSourceA          = 2'd0; //
+            storeControl        = 2'd0;
+            loadSizeControl     = 2'd0;
+            shamtControl        = 2'd0; 
+            shiftSourceControl  = 1'd0;
+            ALULogic            = 2'd0; 
+
+            IorD                = 3'd0;
+            MemToReg            = 3'd0; 
+            ALUSourceB          = 3'd1; //
+            AluOp               = 3'd2; // 
+            ShiftControl        = 3'd0;
+            PCSource            = 3'd0; //
+
+            STATE = ATRASA_PROX_INSTR;
+        end
+
 //////////////////////////////////////////////////////////////////////////////////////
         else if(STATE == ADDI) begin
             PC_write            = 1'd0;
