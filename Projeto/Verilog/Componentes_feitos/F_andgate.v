99d5d63e3c926a13decc4cd01c899aa7e606133f
Daniel Cavalcante <dvictor.cintra@gmail.com>
2022-04-06 23:24:25 -0300

    Portas NOT e AND removidas

--- Projeto/Verilog/Componentes_feitos/F_andgate.v
+++
@@ -1,9 +0,0 @@
-module F_andgate(
-    input  wire  mux_branch_out,
-    input  wire  PC_write_cond,
-    output wire  and_out
-);
-
-assign and_out = mux_branch_out & PC_write_cond;
-
-endmodule
--- Projeto/Verilog/Componentes_feitos/F_notgate.v
+++
@@ -1,9 +0,0 @@
-module F_notgate (
-    input wire     data_in,
-    output wire    data_out
-);
-
-assign data_out  =  !data_in;
-//assign data_out = (data_in) ? 1'b0 : 1'b1;
-
-endmodule
