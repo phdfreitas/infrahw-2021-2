f048034d74ebbfe281137189d32ba5208e52013e
phdfreitas <pedro.hnd.freitas@gmail.com>
2022-04-07 08:51:12 -0300

    SW and LW done

--- Projeto/Verilog/Componentes_feitos/F_ctrl_unit.v
+++ Projeto/Verilog/Componentes_feitos/F_ctrl_unit.v
@@ -50,123 +50,126 @@ reg [6:0] STATE;
 
 // Constantes (Se refere a cada estado)
 // --------- COMMOM STEPS --------- 
-parameter RESET             = 6'd0;
-parameter INSTRUCTION_FETCH = 6'd1;
-parameter WAIT              = 6'd2;
-parameter WAIT_2            = 6'd3;
-parameter DECODE            = 6'd4;
-parameter DECODE_WAIT       = 6'd5; // Serve pra fechar a escrita nos REG_A e REG_B antes de começar qualquer outra coisa
+parameter RESET             = 7'd0;
+parameter INSTRUCTION_FETCH = 7'd1;
+parameter WAIT              = 7'd2;
+parameter WAIT_2            = 7'd3;
+parameter DECODE            = 7'd4;
+parameter DECODE_WAIT       = 7'd5; // Serve pra fechar a escrita nos REG_A e REG_B antes de começar qualquer outra coisa
 
 // ----- R FORMAT INSTRUCTIONS -----
-parameter ADD               = 6'd6;
-parameter AND               = 6'd7;
-parameter SUB               = 6'd8;
-parameter DIV               = 6'd10;
-parameter MULT              = 6'd11;
-parameter JR                = 6'd12;
-parameter MFHI              = 6'd13;
-parameter MFLO              = 6'd14;
-parameter SLL               = 6'd15;
-parameter SLLV              = 6'd16;
-parameter SLT               = 6'd17;
-parameter SRA               = 6'd18;
-parameter SRAV              = 6'd19;
-parameter SRL               = 6'd20;
-parameter BREAK             = 6'd21;
-parameter RTE               = 6'd22;
-parameter ADDM              = 6'd23;
+parameter ADD               = 7'd6;
+parameter AND               = 7'd7;
+parameter SUB               = 7'd8;
+parameter DIV               = 7'd10;
+parameter MULT              = 7'd11;
+parameter JR                = 7'd12;
+parameter MFHI              = 7'd13;
+parameter MFLO              = 7'd14;
+parameter SLL               = 7'd15;
+parameter SLLV              = 7'd16;
+parameter SLT               = 7'd17;
+parameter SRA               = 7'd18;
+parameter SRAV              = 7'd19;
+parameter SRL               = 7'd20;
+parameter BREAK             = 7'd21;
+parameter RTE               = 7'd22;
+parameter ADDM              = 7'd23;
 //
 // ----- I FORMAT INSTRUCTIONS -----
-parameter ADDI              = 6'd24;
-parameter ADDIU             = 6'd25;
-parameter BEQ               = 6'd26;
-parameter BNE               = 6'd27;
-parameter BLE               = 6'd28;
-parameter BGT               = 6'd29;
-parameter SLLM              = 6'd30;
-parameter LB                = 6'd31;
-parameter LH                = 6'd32;
-parameter LUI               = 6'd33;
-parameter LW                = 6'd34;
-parameter SB                = 6'd35;
-parameter SH                = 6'd36;
-parameter SLTI              = 6'd37;
-parameter SW                = 6'd38;
+parameter ADDI              = 7'd24;
+parameter ADDIU             = 7'd25;
+parameter BEQ               = 7'd26;
+parameter BNE               = 7'd27;
+parameter BLE               = 7'd28;
+parameter BGT               = 7'd29;
+parameter SLLM              = 7'd30;
+parameter LB                = 7'd31;
+parameter LH                = 7'd32;
+parameter LUI               = 7'd33;
+parameter LW                = 7'd34;
+parameter SB                = 7'd35;
+parameter SH                = 7'd36;
+parameter SLTI              = 7'd37;
+parameter SW                = 7'd38;
 //
 // ----- J FORMAT INSTRUCTIONS -----
-parameter J                 = 6'd39;
-parameter JAL               = 6'd40;
+parameter J                 = 7'd39;
+parameter JAL               = 7'd40;
 //
 // ----- END AND EXTRAS INSTRUCTIONS -----
-parameter END_ADD_SUB_AND   = 6'd41;
-parameter END_IMMEDIATE     = 6'd42;
-parameter ATRASA_PROX_INSTR = 6'd43; // Monitor tinha falado pra retardar alguma instrução, agora todas são retartadas em 1 step
-parameter SHIFT_WITH_SHAMT  = 6'd44;
-parameter SHIFT_WITH_RT     = 6'd45;
-parameter END_SHIFT         = 6'd46;
-parameter SHIFT_WAIT        = 6'd47;
-parameter END_SLT_SLTI      = 6'd48;
-parameter END_BEQ           = 6'd54;
-parameter END_BNE           = 6'd55;
-parameter END_BLE           = 6'd56;
-parameter END_BGT           = 6'd57;
+parameter END_ADD_SUB_AND   = 7'd41;
+parameter END_IMMEDIATE     = 7'd42;
+parameter ATRASA_PROX_INSTR = 7'd43; // Monitor tinha falado pra retardar alguma instrução, agora todas são retartadas em 1 step
+parameter SHIFT_WITH_SHAMT  = 7'd44;
+parameter SHIFT_WITH_RT     = 7'd45;
+parameter END_SHIFT         = 7'd46;
+parameter SHIFT_WAIT        = 7'd47;
+parameter END_SLT_SLTI      = 7'd48;
+parameter END_BEQ           = 7'd54;
+parameter END_BNE           = 7'd55;
+parameter END_BLE           = 7'd56;
+parameter END_BGT           = 7'd57;
 
-parameter LOAD_STORE_COMMON = 6'd49;
-parameter LOAD_BASIC_STEP   = 6'd50;
-parameter LOAD_INTER_STEP   = 6'd51;
-parameter LOAD_FINAL_STEP   = 6'd52;
-parameter LW_FINAL          = 6'd53;
-parameter STORE_BASIC_STEP  = 6'd58;
-parameter STORE_INTER_STEP  = 6'd59;
-parameter STORE_FINAL_STEP  = 6'd60;
-parameter SW_FINAL          = 6'd61;
-parameter ADDM_READ_WAIT    = 6'd62;
-parameter ADDM_NEXT_READ    = 6'd63;
+// ---------- Load Control ----------
+parameter LOAD_STORE_COMMON = 7'd49;
+parameter LOAD_BASIC_STEP   = 7'd50;
+parameter LOAD_INTER_STEP   = 7'd51;
+parameter LOAD_FINAL_STEP   = 7'd52;
+parameter LW_FINAL          = 7'd53;
+
+// ---------- Store Control ----------
+parameter STORE_BASIC_STEP  = 7'd58;
+parameter STORE_INTER_STEP  = 7'd59;
+parameter STORE_FINAL_STEP  = 7'd60;
+parameter SW_FINAL          = 7'd61;
+parameter ADDM_READ_WAIT    = 7'd62;
+parameter ADDM_NEXT_READ    = 7'd63;
 parameter ADDM_READ_WAIT2   = 7'd64;
 parameter ADDM_FINAL        = 7'd65;
 
 //
 //
 // ----- OPCODE AND FUNCT VALUE (R FORMAT INSTRUCTIONS) -----
-parameter R_FORMAT_OPCODE    = 6'h0;
-parameter R_FORMAT_ADD       = 6'h20;
-parameter R_FORMAT_AND       = 6'h24;
-parameter R_FORMAT_DIV       = 6'h1A;
-parameter R_FORMAT_MULT      = 6'h18;
-parameter R_FORMAT_JR        = 6'h8;
-parameter R_FORMAT_MFHI      = 6'h10;
-parameter R_FORMAT_MHLO      = 6'h12;
-parameter R_FORMAT_SLL       = 6'h0;
-parameter R_FORMAT_SLLV      = 6'h4;
-parameter R_FORMAT_SLT       = 6'h2A;
-parameter R_FORMAT_SRA       = 6'h3;
-parameter R_FORMAT_SRAV      = 6'h7;
-parameter R_FORMAT_SRL       = 6'h2;
-parameter R_FORMAT_SUB       = 6'h22;
-parameter R_FORMAT_BREAK     = 6'hD;
-parameter R_FORMAT_RTE       = 6'h13;
-parameter R_FORMAT_ADDM      = 6'h5;
+parameter R_FORMAT_OPCODE    = 7'h0;
+parameter R_FORMAT_ADD       = 7'h20;
+parameter R_FORMAT_AND       = 7'h24;
+parameter R_FORMAT_DIV       = 7'h1A;
+parameter R_FORMAT_MULT      = 7'h18;
+parameter R_FORMAT_JR        = 7'h8;
+parameter R_FORMAT_MFHI      = 7'h10;
+parameter R_FORMAT_MHLO      = 7'h12;
+parameter R_FORMAT_SLL       = 7'h0;
+parameter R_FORMAT_SLLV      = 7'h4;
+parameter R_FORMAT_SLT       = 7'h2A;
+parameter R_FORMAT_SRA       = 7'h3;
+parameter R_FORMAT_SRAV      = 7'h7;
+parameter R_FORMAT_SRL       = 7'h2;
+parameter R_FORMAT_SUB       = 7'h22;
+parameter R_FORMAT_BREAK     = 7'hD;
+parameter R_FORMAT_RTE       = 7'h13;
+parameter R_FORMAT_ADDM      = 7'h5;
 
 // ----- OPCODE (I FORMAT INSTRUCTIONS) -----
-parameter I_FORMAT_ADDI    = 6'h8;
-parameter I_FORMAT_ADDIU   = 6'h9;
-parameter I_FORMAT_BEQ     = 6'h4;
-parameter I_FORMAT_BNE     = 6'h5;
-parameter I_FORMAT_BLE     = 6'h6;
-parameter I_FORMAT_BGT     = 6'h7;
-parameter I_FORMAT_SLLM    = 6'h1;
-parameter I_FORMAT_LB      = 6'h20;
-parameter I_FORMAT_LH      = 6'h21;
-parameter I_FORMAT_LUI     = 6'hF;
-parameter I_FORMAT_LW      = 6'h23;
-parameter I_FORMAT_SB      = 6'h28;
-parameter I_FORMAT_SH      = 6'h29;
-parameter I_FORMAT_SLTI    = 6'hA;
-parameter I_FORMAT_SW      = 6'h2B;
+parameter I_FORMAT_ADDI    = 7'h8;
+parameter I_FORMAT_ADDIU   = 7'h9;
+parameter I_FORMAT_BEQ     = 7'h4;
+parameter I_FORMAT_BNE     = 7'h5;
+parameter I_FORMAT_BLE     = 7'h6;
+parameter I_FORMAT_BGT     = 7'h7;
+parameter I_FORMAT_SLLM    = 7'h1;
+parameter I_FORMAT_LB      = 7'h20;
+parameter I_FORMAT_LH      = 7'h21;
+parameter I_FORMAT_LUI     = 7'hF;
+parameter I_FORMAT_LW      = 7'h23;
+parameter I_FORMAT_SB      = 7'h28;
+parameter I_FORMAT_SH      = 7'h29;
+parameter I_FORMAT_SLTI    = 7'hA;
+parameter I_FORMAT_SW      = 7'h2B;
 
 // ----- OPCODE (J FORMAT INSTRUCTIONS) -----
-parameter J_FORMAT_JUMP    = 6'h2;
-parameter J_FORMAT_JAL     = 6'h3;
+parameter J_FORMAT_JUMP    = 7'h2;
+parameter J_FORMAT_JAL     = 7'h3;
 
 initial begin
     STATE = INSTRUCTION_FETCH;
@@ -1142,7 +1145,7 @@ always @(posedge clk) begin
             RegWrite            = 1'd0;
             A_write             = 1'd0;
             B_write             = 1'd0;
-            MDR_load            = 1'd1;
+            MDR_load            = 1'd1; //
             EPCWrite            = 1'd0;
             AluOutWrite         = 1'd0; //
 
@@ -1202,7 +1205,7 @@ always @(posedge clk) begin
             B_write             = 1'd0;
             MDR_load            = 1'd0;
             EPCWrite            = 1'd0;
-            AluOutWrite         = 1'd1;
+            AluOutWrite         = 1'd0; //
 
             RegDst              = 2'd0;
             ALUSourceA          = 2'd1;
@@ -1211,7 +1214,7 @@ always @(posedge clk) begin
             shamtControl        = 2'd0;
             shiftSourceControl  = 1'd0;
 
-            IorD                = 3'd0; //
+            IorD                = 3'd6; //
             MemToReg            = 3'd0;
             ALUSourceB          = 3'd3; 
             AluOp               = 3'd1;
